4ba43ddced931ab3c668379f0050ddf1d9b2befe
Julia Lazari <juliaplazari@gmail.com>
2023-06-13 22:31:34 -0300

    Código funcional e comentado do módulo 2

--- Projeto2/Modulo2_projeto2/Modulo2_projeto2.ino
+++ Projeto2/Modulo2_projeto2/Modulo2_projeto2.ino
@@ -2,10 +2,10 @@
 
 #include <LiquidCrystal.h>
 // Display 16x2
-const int rs = 6, en = 5, d4 = 4, d5 = 3, d6 = 2, d7 = 1; //configura os pinos que serão usados no display
+const int rs = 6, en = 5, d4 = 4, d5 = 3, d6 = 2, d7 = A0; //configura os pinos que serão usados no display
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
-// Teclado matricial
+// Pinos associados ao teclado matricial
 const int row_1 = 7;
 const int row_2 = 8;
 const int row_3 = 9;
@@ -15,26 +15,27 @@ const int col_2 = 12;
 const int col_3 = 13;
 
 //Timer 0
-int count_ISR =0;
-int count;
-int count_50;
+int volatile count_ISR =0;
 
-// Variáveis
-int estado;
-int tempo;
-boolean flag_tecla;
-int tecla;
+// Declaração das variáveis
+int estado; // estado para a máquina de estados
+int volatile tempo; // variável auxiliar para debounce
+boolean flag_tecla; // flag para informar se identificou tecla
+String tecla; // variável para qual tecla foi identificada
+int displayed = 0; // número de caracteres que foi mostrado no display
+//Arrays
 int C[3]; //array para informação das colunas
 int Ca[3]; // array para mudança na coluna
-int C_ones[3] = {1,1,1}; //array de uns (colunas em nível alto)
-int L[4];
+int C_ones[3] = {1,1,1}; //array de uns (colunas em nível alto) para comparação
+int L[4]; //array para informação das linhas
 
+//--------------------------------------SETUP----------------------------------------
 void setup() {
   // configura interrupções periódicas associadas ao timer 0
   configuracao_Timer0();
   sei();// permite interruções
 
-  //configura pinos do teclado matricial
+  //Configura pinos do teclado matricial
   // linhas são saídas e colunas são entradas
   pinMode(row_1,OUTPUT);
   pinMode(row_2,OUTPUT);
@@ -44,10 +45,15 @@ void setup() {
   pinMode(col_2,INPUT);
   pinMode(col_3,INPUT);
 
-  // display 16x2
+  // Display 16x2
   lcd.begin (16,2); //inicializa
-  lcd.setCursor (0,0);
   lcd.print("Hello"); // printa mensagem inicial
+  lcd.setCursor(0, 1); // cursor na segunda linha
+  
+  // Interface Serial
+  Serial.begin(9600);
+  Serial.println("oi");
+  
 }
 
 void configuracao_Timer0(){
@@ -83,77 +89,48 @@ ISR(TIMER0_COMPA_vect){// a cada interrupção periódica do timer 0 (a cada 1.6
   } 
 
 
+void tecla_para_display(String tecla){
+  // Identifica a tecla e printa
+  // O display inicia com Hello na primeira linha e dando display
+  // dos caracteres digitados na segunda linha
 
-void display_16x2(String message){
-  // Mensagem no display 16x2
-  // display não será apagado antes de mostrar próxima mensagem
-  // para visualizar sequência de comandos e garantir que debounce foi
-  // implementado corretamente
-  //lcd.clear();
-  
-  lcd.print(message);
+  // Quando completa segunda linha, apaga a primeira e coloca o cursor nela
+  if(displayed==16){
+    lcd.setCursor(0, 0); 
+    lcd.println("                ");
+    lcd.setCursor(0, 0); 
+  }
+
+  // Quando completa primeira linha, apaga a segunda e coloca o cursor nela
+  if(displayed==32){
+    lcd.setCursor(0, 1); 
+    lcd.println("                ");
+    displayed = 0;
+    lcd.setCursor(0, 1); 
+  }
+
+  //Display da tecla identificada
+  lcd.print(tecla);
+  displayed ++; // increnta variável que indica quantos caracteres já foram no display
 }
 
-void estado_para_display(int tecla){
-  if(tecla==0){
-    display_16x2("0");
-  }
-
-  else if(tecla==1){
-    display_16x2("1");
-  }
-  
-  else if(tecla==2){
-    display_16x2("2");
-  }
-  
-  else if(tecla==3){
-    display_16x2("3");
-  }
-  
-  else if(tecla==4){
-    display_16x2("4");
-  }
-  
-  else if(tecla==5){
-    display_16x2("5");
-  }
-  
-  else if(tecla==6){
-    display_16x2("6");
-  }
-  
-  else if(tecla==7){
-    display_16x2("7");
-  }
-  
-  else if(tecla==8){
-    display_16x2("8");
-  }
-  
-  else if(tecla==9){
-    display_16x2("9");
-  }
-  
-  else if(tecla==10){//*
-    display_16x2("*");
-  }
-
-  else if(tecla==11){//#
-    display_16x2("#");
-  }
-}
-
+// Função auxiliar que compara arrays e retorna true se todos elementos forem iguais
 bool compareArrays(int array1[], int array2[], int size) {
   for (int i = 0; i < size; i++) {
-    if (array1[i] != array2[i]) {
-      return false;
+    if (array1[i] != array2[i]) { // caso algum elemento seja diferente
+      return false; // retorna false
     }
   }
-  return true;
+  return true; // se nenhum elemento foi diferente, retorna true
 }
 
-int descobre_tecla(int Ca[3],int L[4]){
+// Função para descobrir qual tecla foi apertada
+String descobre_tecla(int Ca[3],int L[4]){
+  // Variáveis de entrada são
+  // Array L com o estado da linha
+  // Array Ca com o estado das colunas no momento da mudança
+
+  // Arrays auxiliares para indicar linhas e colunas específicas
   int arrL1[] = {0,1,1,1};
   int arrL2[] = {1,0,1,1};
   int arrL3[] = {1,1,0,1};
@@ -163,177 +140,248 @@ int descobre_tecla(int Ca[3],int L[4]){
   int arrCa2[] = {1,0,1}; 
   int arrCa3[] = {1,1,0};
 
+  // Compara arrays de linha e coluna com os auxiliares para identificar a tecla acionada
   if(compareArrays(L, arrL1,4) && compareArrays(Ca,arrCa1,3)){
-    tecla = 1;
+    tecla = "1";
   }
 
   else if(compareArrays(L, arrL1,4) && compareArrays(Ca,arrCa2,3)){
-    tecla = 2;
+    tecla = "2";
   }
 
   else if(compareArrays(L, arrL1,4) && compareArrays(Ca,arrCa3,3)){
-    tecla = 3;
+    tecla = "3";
   }
 
   else if(compareArrays(L, arrL2,4) && compareArrays(Ca,arrCa1,3)){
-    tecla = 4;
+    tecla = "4";
   }
   
   else if(compareArrays(L, arrL2,4) && compareArrays(Ca,arrCa2,3)){
-    tecla = 5;
+    tecla = "5";
   }
 
   else if(compareArrays(L, arrL2,4) && compareArrays(Ca,arrCa3,3)){
-    tecla = 6;
+    tecla = "6";
   }
 
   else if(compareArrays(L, arrL3,4) && compareArrays(Ca,arrCa1,3)){
-    tecla = 7;
+    tecla = "7";
   }
 
   else if(compareArrays(L, arrL3,4) && compareArrays(Ca,arrCa2,3)){
-    tecla = 8;
+    tecla = "8";
   }
 
   else if(compareArrays(L, arrL3,4) && compareArrays(Ca,arrCa3,3)){
-    tecla = 9;
+    tecla = "9";
   }
 
   else if(compareArrays(L, arrL4,4) && compareArrays(Ca,arrCa1,3)){
-    tecla = 10;
+    tecla = "*";
   }
 
   else if(compareArrays(L, arrL4,4) && compareArrays(Ca,arrCa2,3)){
-    tecla = 0;
+    tecla = "0";
   }
 
   else if(compareArrays(L, arrL4,4) && compareArrays(Ca,arrCa3,3)){
-    tecla = 11;
+    tecla = "#";
   }
 
   return tecla;
 }
 
 void loop() {
-
+  // Máquina de estados que realiza varredura para identificar quando uma tecla foi apertada,
+  // debounce, e display da tecla no LCD 16x2
   switch(estado){
-      case 0:
-        C[0] = col_1;
-        C[1] = col_2;
-        C[2] = col_3;
-        if(compareArrays(C, C_ones,3)){
-          estado = 1;
+      case 0: // estado 0
+        // L = [0111] => primeira linha como baixo
+
+        // lê valores das colunas
+        C[0] = digitalRead(col_1);
+        C[1] = digitalRead(col_2);
+        C[2] = digitalRead(col_3);
+    	
+        // caso todas colunas estejam em alto
+        if(compareArrays(C, C_ones,3)==1){
+          estado = 1; // vai para o próximo estado
+          // L = [1011] => segunda linha como baixo
           digitalWrite(row_1,HIGH);
           digitalWrite(row_2,LOW);
           digitalWrite(row_3,HIGH);
           digitalWrite(row_4,HIGH);
         }
 
-        if(compareArrays(C, C_ones,3)==false){
-          estado = 4;          
+        // caso contrário, salva o estado das colunas em Ca
+        // e avança para o estado 4
+        else{
+        Ca[0] = digitalRead(col_1);
+        Ca[1] = digitalRead(col_2);
+        Ca[2] = digitalRead(col_3);
+        estado = 4;          
         }
+    
+    	break;
 
       case 1:
-        C[0] = col_1;
-        C[1] = col_2;
-        C[2] = col_3;
-        if(compareArrays(C, C_ones,3)){
-          estado = 2;
+    	  // L = [1011] => segunda linha como baixo
+        C[0] = digitalRead(col_1);
+        C[1] = digitalRead(col_2);
+        C[2] = digitalRead(col_3);
+    
+        // caso todas colunas estejam em alto
+        if(compareArrays(C, C_ones,3)==1){
+          estado = 2; // vai para o próximo estado
+          // L = [1101] => terceira linha como baixo
           digitalWrite(row_1,HIGH);
           digitalWrite(row_2,HIGH);
           digitalWrite(row_3,LOW);
           digitalWrite(row_4,HIGH);
         }
 
-        if(compareArrays(C, C_ones,3) ==  false){
-          estado = 4;
+        // caso contrário, salva o estado das colunas em Ca
+        // e avança para o estado 4
+        else{
+        Ca[0] = digitalRead(col_1);
+        Ca[1] = digitalRead(col_2);
+        Ca[2] = digitalRead(col_3);
+        estado = 4;
         }
+    
+    	break;
 
       case 2:
-        C[0] = col_1;
-        C[1] = col_2;
-        C[2] = col_3;
-
-        if(compareArrays(C, C_ones,3)){
-          estado = 3;
+        // L = [1101] => terceira linha como baixo
+        C[0] = digitalRead(col_1);
+        C[1] = digitalRead(col_2);
+        C[2] = digitalRead(col_3);
+    
+        // caso todas colunas estejam em alto
+        if(compareArrays(C, C_ones,3)==1){
+          estado = 3; // vai para o próximo estado
+          // L = [1110] => quarta linha como baixo
           digitalWrite(row_1,HIGH);
           digitalWrite(row_2,HIGH);
           digitalWrite(row_3,HIGH);
           digitalWrite(row_4,LOW);
         }
 
-        if(compareArrays(C, C_ones,3) ==  false){
-          estado = 4;
+        // caso contrário, salva o estado das colunas em Ca
+        // e avança para o estado 4
+        else{
+        Ca[0] = digitalRead(col_1);
+        Ca[1] = digitalRead(col_2);
+        Ca[2] = digitalRead(col_3);
+        estado = 4;
         }
+    
+    	break;
         
       case 3:
-        C[0] = col_1;
-        C[1] = col_2;
-        C[2] = col_3;
+    	  // L = [1110] => quarta linha como baixo
+        C[0] = digitalRead(col_1);
+        C[1] = digitalRead(col_2);
+        C[2] = digitalRead(col_3);
 
-        if(compareArrays(C, C_ones,3)){
+        // caso todas colunas estejam em alto
+        if(compareArrays(C, C_ones,3)==1){
           estado = 0;
+          // L = [0111] => primeira linha como baixo
           digitalWrite(row_1,LOW);
           digitalWrite(row_2,HIGH);
           digitalWrite(row_3,HIGH);
           digitalWrite(row_4,HIGH);
         }
 
-        if(compareArrays(C, C_ones,3) == false){
-          Ca[0] = col_1;
-          Ca[1] = col_2;
-          Ca[2] = col_3;
+        // caso contrário, salva o estado das colunas em Ca
+        // e avança para o estado 4
+        else{
+        Ca[0] = digitalRead(col_1);
+        Ca[1] = digitalRead(col_2);
+        Ca[2] = digitalRead(col_3);
 
-          estado = 4;
+        estado = 4;
         }
+    
+    	break;
 
       case 4:
-        
-        estado = 5;
+        // estado intermediário quando alguma mudança foi identificada
+        estado = 5; // vai para o estado 5 realizar o debounce
+    	break;
 
       case 5:
-        // debounce
-        tempo = count_ISR;
-        while(count_ISR < tempo+30){
-          //VER SE PRECISA DE ALGO CASO NAO FUNCIONE
+        // estado para o debounce
+        tempo = count_ISR; // salva valor do contador das interrupções do timer 0
+        while(count_ISR < tempo+30){ // espera esse valor incrementar 30 (~50ms)
+        // para checar se a mudança realmente ocorreu
         }
-        if(compareArrays(C, Ca,3)){
-          estado = 6;
+
+        // se tecla realmente foi apertada
+        if(compareArrays(C, Ca,3)==1){
+          estado = 6; // vai para o estado 6
           }
-        if(compareArrays(C, Ca,3) == false){
-          estado = 0;
+
+        // se tecla não foi apertada
+        else if(compareArrays(C, Ca,3) == 0){
+          estado = 0; // retorna para a varredura
           }
+    	break;
 
       case 6:
-        tecla = descobre_tecla(Ca,L);
-        flag_tecla = 1;
-        estado = 7;
+    	// estado quando uma mudança foi identificada
+      // e é preciso identificar qual foi a tecla apertada
+
+      // salva estado das linhas
+      L[0] = digitalRead(row_1);
+      L[1] = digitalRead(row_2);
+      L[2] = digitalRead(row_3);
+      L[3] = digitalRead(row_4);
+
+      // chama função para identificar qual a tecla
+      tecla = descobre_tecla(Ca,L);
+      // flag que indica que uma tecla foi identificada
+      flag_tecla = 1;
+      estado = 7; // avança para o próximo estado
+    	break;
       
       case 7:
-        L[0] = row_1;
-        L[1] = row_2;
-        L[2] = row_3;
-        L[3] = row_4;
-        
-        if(compareArrays(C, C_ones,3)){
+    	  // lê estado das colunas
+        C[0] = digitalRead(col_1);
+        C[1] = digitalRead(col_2);
+        C[2] = digitalRead(col_3);
+
+        // se todas retornaram para o nível alto vai para o estado 8
+        if(compareArrays(C, C_ones,3)==1){
           estado = 8;
         }
-        if(compareArrays(C, C_ones,3) == false){
+        // se tecla ainda está apertada permanece nesse estado
+        if(compareArrays(C, C_ones,3) == 0){
           estado = 7;
         }
+    	break;
 
       case 8:
-        tempo = count_ISR;
-        while(count_ISR < tempo+30){
-          //VER SE PRECISA DE ALGO CASO NAO FUNCIONE
+    	  // Debounce análogo ao estado 5
+        // com o objetivo de identificar se a tecla foi solta
+        tempo = count_ISR; // salva valor do contador das interrupções do timer 0
+        while(count_ISR < tempo+30){ // espera esse valor incrementar 30 (~50ms)
+        // para checar se a mudança realmente ocorreu
         }
-        if(compareArrays(C, C_ones,3)){
-          estado_para_display(tecla);
-          estado = 0;
+
+        // se tecla foi solta        
+        if(compareArrays(C, C_ones,3)==1){
+          tecla_para_display(tecla); // mostra valor da tecla no display
+          estado = 0; // retorna para a varredura
           }
-        if(compareArrays(C, C_ones,3) == false){
-          estado = 7;
+
+        // se tecla não foi solta
+        if(compareArrays(C, C_ones,3) == 0){
+          estado = 7; // retorna para o estado anterior
           }
+    	break;
   }
   
 }
